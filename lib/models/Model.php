5f2150f9b044a906eef2523f8282bc605b81f297
h-z <hz@muszaki.info>
2012-01-03 21:30:42 +0000

    model w/ identity map
    git-svn-id: svn+ssh://lhc.muszaki.info/home/hz/svn/php/kms@22 ee783909-867a-4e75-a3a9-9be0a920c0ae

---
+++ lib/models/Model.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * User: HZ
+ * Date: 2012.01.03.
+ */
+class Model {
+    protected static $instances = array();
+
+    protected $id;
+
+    public function __construct() {
+        $this->store();
+
+    }
+
+    protected function store() {
+        self::$instances[$this->id] = $this;
+    }
+
+    protected function load() {
+
+    }
+
+    protected function _generateKey($id) {
+        return get_class($this) . '-' . $id;
+    }
+
+
+    public function identify() {
+
+    }
+
+    public function find($id) {
+        if (isset(self::$instances[$id])) {
+            return self::$instances[$id];
+        }
+
+    }
+
+
+}
