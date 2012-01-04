e7e9dc08189e31da7cde363438ff7f96b64cccd1
h-z <hz@muszaki.info>
2012-01-04 23:54:42 +0000

    core autoload
    git-svn-id: svn+ssh://lhc.muszaki.info/home/hz/svn/php/kms@27 ee783909-867a-4e75-a3a9-9be0a920c0ae

--- lib/system/Core.php
+++ lib/system/Core.php
@@ -9,57 +9,59 @@ session_start();
 
 class Core {
 
-  public function __construct() {
-  }
+    private static $loadedDirectories;
 
-  public function page($type = '') {
-
-  }
-
-  private static function getdirs($dir) {
-    if (!is_dir($dir)) {
-      return;
+    public function __construct() {
+        spl_autoload_register(array('Core', 'autoloader'));
     }
-    $handle = opendir($dir);
-    $subdirs = array();
-    while (($file = readdir($handle)) !== false) {
-      if (is_dir($file) && !in_array($file, array('.', '..'))) {
-        $subdirs[$file] = self::getdirs($file);
-      }
-    }
-    closedir($handle);
-    return $subdirs;
-  }
 
-  private static function createpaths($pre, $dirs) {
-    print_r($dirs);
-    $paths = array();
-    foreach($dirs as $d => $subs) {
-      $paths[] = $pre.'/'.$d;
-      if ($subs) {
-        $paths = array_merge($paths, self::createpaths($pre.'/'.$d, $subs));
-      }
-    }
-    return $paths;
-  }
+    public function page($type = '') {
 
-  public static function autoloadpaths() {
-    return self::createpaths('.', self::getdirs('.'));
-  }
+    }
+
+    private static function getdirs($dir) {
+        if (!is_dir($dir)) {
+            return null;
+        }
+        $handle = opendir($dir);
+        $subdirs = array();
+        while (($file = readdir($handle)) !== false) {
+            if (is_dir($file) && !in_array($file, array('.', '..'))) {
+                $subdirs[$file] = self::getdirs($file);
+            }
+        }
+        closedir($handle);
+        return $subdirs;
+    }
+
+    private static function createpaths($pre, $dirs) {
+        print_r($dirs);
+        $paths = array();
+        foreach($dirs as $d => $subs) {
+            $paths[] = $pre.'/'.$d;
+            if ($subs) {
+                $paths = array_merge($paths, self::createpaths($pre.'/'.$d, $subs));
+            }
+        }
+        return $paths;
+    }
+
+    private static function autoloadpaths() {
+    }
+
+    public static function autoloader($className) {
+        //$className = strtolower($className);
+        if (self::$loadedDirectories == null) {
+            self::$loadedDirectories == self::createpaths('.', self::getdirs('.'));
+        }
+
+        foreach (self::$loadedDirectories as $dir) {
+            $file = $dir.'/'.$className.'.php';
+            if ( file_exists($file) ) {
+                require_once($file);
+            }
+        }
+    }
 }
-
-function __autoload($className) {
-  //$className = strtolower($className);
-
-  $directories = Core::autoloadpaths();
-  foreach ($directories as $dir) {
-    $file = $dir.'/'.$className.'.php';
-    if ( file_exists($file) ) {
-      require_once($file);
-    }
-  }
-}
-
-
 
 
