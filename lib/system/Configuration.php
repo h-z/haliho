ba29270b7b212b2e5787fae7d81bb2825609debe
h-z <hz@muszaki.info>
2012-01-05 21:58:57 +0000

    config
    git-svn-id: svn+ssh://lhc.muszaki.info/home/hz/svn/php/kms@46 ee783909-867a-4e75-a3a9-9be0a920c0ae

--- lib/system/Configuration.php
+++ lib/system/Configuration.php
@@ -1,34 +1,33 @@
 <?php
-  class Configuration extends Singleton {
+class Configuration extends Singleton {
     private $values;
 
     public function __construct() {
-      $dom = new DOMDocument();
-      $dom->load('../configuration/configuration.xml');
-      if ($dom->documentElement->hasChildNodes()) {
-      foreach ($dom->documentElement->childNodes as $childNode) {
-          if ($childNode->nodeType == XML_ELEMENT_NODE) {
-            $this->values[$childNode->nodeName] = $childNode->nodeValue;
-          }
+        $dom = new DOMDocument();
+        $dom->load('../configuration/configuration.xml');
+        if ($dom->documentElement->hasChildNodes()) {
+            foreach ($dom->documentElement->childNodes as $childNode) {
+                if ($childNode->nodeType == XML_ELEMENT_NODE) {
+                    $this->values[$childNode->nodeName] = $childNode->nodeValue;
+                }
+            }
         }
-      }
-      print_r($this->values);
     }
 
     public function get($key) {
-      if (isset($this->values[$key])) {
-        return $this->values[$key];
-      } else {
-        return null;
-      }
+        if (isset($this->values[$key])) {
+            return $this->values[$key];
+        } else {
+            return null;
+        }
     }
 
     public function __get($key) {
-      return $this->get($key);
+        return $this->get($key);
     }
-   
-    
-  }
+
+
+}
 
 
 
