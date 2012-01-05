860bb3865d325305b692e7bf7efcef9e0492245a
h-z <hz@muszaki.info>
2012-01-05 16:08:38 +0000

    sqlite db
    git-svn-id: svn+ssh://lhc.muszaki.info/home/hz/svn/php/kms@32 ee783909-867a-4e75-a3a9-9be0a920c0ae

---
+++ lib/databases/sqlite/SQLiteQuery.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * User: hz
+ * Date: 2011.11.11.
+ */
+ 
+class SQLiteQuery implements IQuery {
+
+  private $result;
+  private $query;
+  private $db;
+
+  function __construct(IDatabase $db, $query = "", $values = array()) {
+    $this->db = $db;
+    $this->query = $query;
+    $this->result = sqlite_query($db->getConnection(), $this->query);
+  }
+
+  function fetchAll() {
+    $r = array();
+    while ($row = $this->row()) {
+      $r[] = $row;
+    }
+    return $r;
+  }
+
+  function fetchAllAssoc() {
+    $r = array();
+    while ($row = $this->arow()) {
+      $r[] = $row;
+    }
+    return $r;
+  }
+
+  function row() {
+    return sqlite_fetch_array($this->result, SQLITE_NUM);
+  }
+
+  function arow() {
+    return sqlite_fetch_array($this->result, SQLITE_ASSOC);
+  }
+
+  function count() {
+    return sqlite_num_rows($this->result);
+  }
+
+  function free() {
+     $this->result = false;
+  }
+}
