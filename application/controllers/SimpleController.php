a542621e4ab7d0b09c80adfb7512418d22a86981
h-z <hz@muszaki.info>
2012-01-08 11:12:03 +0000

    db test
    git-svn-id: svn+ssh://lhc.muszaki.info/home/hz/svn/php/kms@72 ee783909-867a-4e75-a3a9-9be0a920c0ae

--- application/controllers/SimpleController.php
+++ application/controllers/SimpleController.php
@@ -3,19 +3,19 @@
  * User: HZ
  * Date: 2011.12.11.
  */
- 
+
 class SimpleController extends XmlController {
 
-  public function simple() {
-    $my_header = new Header('', Header::SCRIPT, 'alert(1);');
-    $this->addHeader($my_header);
-  }
+    public function simple() {
+        $my_header = new Header('', Header::SCRIPT, 'alert(1);');
+        $this->addHeader($my_header);
+    }
 
-  public function m(DOMNode $node) {
+    public function m(DOMNode $node) {
 
-      $a = $node->ownerDocument->createDocumentFragment();
-      $a->appendXML('<foobar><bar/><foo/></foobar>');
-
-      return $a;
-  }
+        $a = $node->ownerDocument->createDocumentFragment();
+        $a->appendXML('<foobar><bar/><foo/></foobar>');
+        var_dump($this->db);
+        return $a;
+    }
 }
