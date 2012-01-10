82a7cc3941abb2df2891c939ff1721bdc3398b85
h-z <hz@muszaki.info>
2012-01-10 21:15:08 +0000

    singleton refactor
    git-svn-id: svn+ssh://lhc.muszaki.info/home/hz/svn/php/kms@98 ee783909-867a-4e75-a3a9-9be0a920c0ae

--- lib/system/Singleton.php
+++ lib/system/Singleton.php
@@ -8,13 +8,10 @@ abstract class Singleton {
     protected static $instances;
     protected function __construct($opts = array()) {}
 
-        public static function getInstance($opts = array()) {
-            $class = get_called_class();
+    public static function getInstance($opts = array()) {
+        $class = get_called_class();
         if (!isset(self::$instances[$class] )) {
-            //$class = get_called_class();
-            var_dump($class);
             self::$instances[$class] = new $class($opts);
-
         }
         return self::$instances[$class];
     }
