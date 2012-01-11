6707f8f449e8fa2a34062b53a991a19723fdd2d7
h-z <hz@muszaki.info>
2012-01-11 18:51:27 +0000

    file logger
    git-svn-id: svn+ssh://lhc.muszaki.info/home/hz/svn/php/kms@106 ee783909-867a-4e75-a3a9-9be0a920c0ae

--- lib/logger/FileLogWriter.php
+++ lib/logger/FileLogWriter.php
@@ -6,4 +6,16 @@
 
 class FileLogWriter implements ILogWriter {
 
+    private $file;
+    public function __construct(DOMNode $node) {
+        $config = Configuration::getInstance();
+
+        $this->file = $config->rootpath . Util::attr($node, 'file');
+        touch($this->file);
+   }
+
+    public function write($msg = '', $level = '') {
+        $msg = date('Y-m-d H:i:s') . ' [' . $level . '] ' . $msg. "\n";
+        file_put_contents($this->file, $msg, FILE_APPEND);
+    }
 }
