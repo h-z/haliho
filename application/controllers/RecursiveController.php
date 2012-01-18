57c9ecb20676ab2afa499fa53ecf233f537db164
HEGEDUS Zoltan <hz@muszaki.info>
2012-01-18 21:08:22 +0100

    recursive sample controller

--- application/controllers/RecursiveController.php
+++ application/controllers/RecursiveController.php
@@ -3,7 +3,21 @@
  * User: HZ
  * Date: 2011.12.11.
  */
- 
-class RecursiveController extends WebController {
 
+class RecursiveController extends WebController {
+    public static $count = 0;
+    public function o(DOMNode $node) {
+        self::$count++;
+        if (self::$count < 2) {
+            $r = '    <kms:controller class="Recursive" method="o" folder="1" articles="yes" >
+                <title><![CDATA[cím]]></title>
+                <text><![CDATA[szöveg]]></text>
+                </kms:controller>
+                ';
+            $a = $node->ownerDocument->createDocumentFragment();
+            $a->appendXML($r);
+
+            return $a;
+        }
+    }
 }
